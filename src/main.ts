afe0126c320ebc8645c895eabd823fe8c6156b75
Firstbober <firstbober@tutanota.com>
2024-07-30 22:29:45 +0200

    Fixes.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import './style.css'
 import axios from 'axios';
 
-let results_mDex: any[] = [];
-
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-class MangaDex {
+interface Engine {
+  setTags(): void,
+  searchByTitle(title: string): Promise<void>
+  searchByTags(includes: string[], excludes: string[]): Promise<void>,
+  exportToCSV(): Promise<void>
+}
+
+class MangaDex implements Engine {
   baseUrl = 'https://corsproxy.io/?https://api.mangadex.org';
+  results_mDex: any[] = [];
 
   async setTags() {
     await sleep(200);
@@ -53,9 +59,9 @@ class MangaDex {
     let total = resp.data.total;
     let offset = resp.data.offset;
 
-    document.getElementById("info")!.innerText = `Fetching by text... ${current}/${total}  (f5 to stop)`;
+    document.getElementById("info")!.innerText = `Fetching by text... ${current}/${total}  (f5 to stop) (title: ${title})`;
 
-    results_mDex = [...results_mDex, ...resp.data.data];
+    this.results_mDex = [...this.results_mDex, ...resp.data.data];
     while(current < total) {
       await sleep(300);
 
@@ -73,12 +79,12 @@ class MangaDex {
       current += resp.data.data.length;
       offset += resp.data.data.length;
 
-      document.getElementById("info")!.innerText = `Fetching by text... ${current}/${total} (f5 to stop)`;
-      results_mDex = [...results_mDex, ...resp.data.data];
+      document.getElementById("info")!.innerText = `Fetching by text... ${current}/${total} (f5 to stop) (title: ${title})`;
+      this.results_mDex = [...this.results_mDex, ...resp.data.data];
     }
 
-    console.log(results_mDex);
-    document.getElementById("info")!.innerHTML = `<span style="font-size: 2em; color: yellow;">ready to export!</span>`;
+    console.log(this.results_mDex);
+    document.getElementById("info")!.innerHTML = `<span style="font-size: 2em; color: yellow;">ready to export! (title: ${title})</span>`;
   }
   async searchByTags(includes: string[], excludes: string[]) {
     if(includes.length == 0 && excludes.length == 0) {
@@ -102,9 +108,9 @@ class MangaDex {
     let total = resp.data.total;
     let offset = resp.data.offset;
 
-    document.getElementById("info")!.innerText = `Fetching by tags... ${current}/${total} (f5 to stop)`;
+    document.getElementById("info")!.innerText = `Fetching by tags... ${current}/${total} (f5 to stop) (incl: ${includes}, excl: ${excludes})`;
 
-    results_mDex = [...results_mDex, ...resp.data.data];
+    this.results_mDex = [...this.results_mDex, ...resp.data.data];
     while(current < total) {
       await sleep(300);
 
@@ -121,20 +127,179 @@ class MangaDex {
       current += resp.data.data.length;
       offset += resp.data.data.length;
 
-      document.getElementById("info")!.innerText = `Fetching by tags... ${current}/${total} (f5 to stop)`;
-      results_mDex = [...results_mDex, ...resp.data.data];
+      document.getElementById("info")!.innerText = `Fetching by tags... ${current}/${total} (f5 to stop) (incl: ${includes}, excl: ${excludes})`;
+      this.results_mDex = [...this.results_mDex, ...resp.data.data];
     }
 
-    console.log(results_mDex);
-    document.getElementById("info")!.innerHTML = `<span style="font-size: 2em; color: yellow;">ready to export!</span>`;
+    console.log(this.results_mDex);
+    document.getElementById("info")!.innerHTML = `<span style="font-size: 2em; color: yellow;">ready to export! (incl: ${includes}, excl: ${excludes})</span>`;
   }
   async exportToCSV() {
-    if(results_mDex.length == 0) return;
+    if(this.results_mDex.length == 0) {
+      document.getElementById("info")!.innerHTML = `<span style="font-size: 2em; color: red;">There are no results!</span>`;
+    };
 
     let csv_lines: string[] = [
       'title,year,tags,link'
     ];
-    for(const manga of results_mDex) {
+    for(const manga of this.results_mDex) {
+      if(manga.attributes.availableTranslatedLanguages.includes("en")) continue;
+
+      let title: string = manga.attributes.title.en;
+      let tags: string[] = [];
+      let link = `https://mangadex.org/title/${manga.id}`;
+      let year_of_publication = manga.attributes.year;
+
+      for(const tag of manga.attributes.tags) {
+        tags.push(tag.attributes.name.en);
+      }
+
+      csv_lines.push(`"${title.replaceAll("\"", "\"\"")}",${year_of_publication == null ? 0 : year_of_publication},"${tags.join(', ')}","${link}"`);
+    }
+
+    csv_lines = [...new Set(csv_lines)];
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(csv_lines.join('\n')));
+    element.setAttribute('download', 'results.csv');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+}
+
+class MangaUpdates implements Engine {
+  baseUrl = 'https://api.mangaupdates.com/v1';
+  results_mDex: any[] = [];
+
+  async setTags() {
+    await sleep(200);
+
+    const tags = await axios(`${this.baseUrl}/genres`, {
+      withCredentials: false
+    });
+    let selectHtml = '<option value="none">none</option>\n';
+
+    let tagArrays = [];
+
+    for (const tag of tags.data) {
+      tagArrays.push([tag.id, tag.genre]);
+    }
+    tagArrays = tagArrays.sort((a, b) => {
+      if (a[1].toLowerCase() < b[1].toLowerCase()) return -1;
+      if (a[1].toLowerCase() > b[1].toLowerCase()) return 1;
+      return 0;
+    })
+
+    for(const tag of tagArrays) {
+      selectHtml += `<option value="${tag[0]}">${tag[1]}</option>\n`;
+    }
+
+    document.getElementById('includes-tag')!.innerHTML = selectHtml;
+    document.getElementById('excludes-tag')!.innerHTML = selectHtml;
+  }
+  async searchByTitle(title: string) {
+    if(title.trim().length == 0) {
+      document.getElementById("info")!.innerText = `You need to write title to search it!`;
+      return;
+    }
+
+    await sleep(200);
+
+    const resp = await axios.post(`${this.baseUrl}/series/search`, {
+      search: title
+    }, {
+      withCredentials: false
+    });
+
+    console.log(resp.data);
+
+    // let current = resp.data.data.length;
+    // let total = resp.data.total;
+    // let offset = resp.data.offset;
+
+    // document.getElementById("info")!.innerText = `Fetching by text... ${current}/${total}  (f5 to stop) (title: ${title})`;
+
+    // this.results_mDex = [...this.results_mDex, ...resp.data.data];
+    // while(current < total) {
+    //   await sleep(300);
+
+    //   const resp = await axios({
+    //     method: 'GET',
+    //     url: `${this.baseUrl}/manga?limit=10`,
+    //     params: {
+    //       title: title,
+    //       offset: offset,
+    //       limit: 100,
+    //       originalLanguage: ['ja']
+    //     },
+    //   });
+
+    //   current += resp.data.data.length;
+    //   offset += resp.data.data.length;
+
+    //   document.getElementById("info")!.innerText = `Fetching by text... ${current}/${total} (f5 to stop) (title: ${title})`;
+    //   this.results_mDex = [...this.results_mDex, ...resp.data.data];
+    // }
+
+    // console.log(this.results_mDex);
+    // document.getElementById("info")!.innerHTML = `<span style="font-size: 2em; color: yellow;">ready to export! (title: ${title})</span>`;
+  }
+  async searchByTags(includes: string[], excludes: string[]) {
+    if(includes.length == 0 && excludes.length == 0) {
+      document.getElementById("info")!.innerText = `At least one tag must be selected!`;
+      return;
+    }
+
+    await sleep(200);
+
+    const resp = await axios.post(`${this.baseUrl}/series/search`, {
+      search: ""
+    });
+
+    let current = resp.data.data.length;
+    let total = resp.data.total;
+    let offset = resp.data.offset;
+
+    document.getElementById("info")!.innerText = `Fetching by tags... ${current}/${total} (f5 to stop) (incl: ${includes}, excl: ${excludes})`;
+
+    this.results_mDex = [...this.results_mDex, ...resp.data.data];
+    while(current < total) {
+      await sleep(300);
+
+      const resp = await axios({
+        method: 'GET',
+        url: `${this.baseUrl}/manga?limit=100`,
+        params: {
+          includedTags: includes,
+          excludedTags: excludes,
+          originalLanguage: ['ja']
+        },
+      });
+
+      current += resp.data.data.length;
+      offset += resp.data.data.length;
+
+      document.getElementById("info")!.innerText = `Fetching by tags... ${current}/${total} (f5 to stop) (incl: ${includes}, excl: ${excludes})`;
+      this.results_mDex = [...this.results_mDex, ...resp.data.data];
+    }
+
+    console.log(this.results_mDex);
+    document.getElementById("info")!.innerHTML = `<span style="font-size: 2em; color: yellow;">ready to export! (incl: ${includes}, excl: ${excludes})</span>`;
+  }
+  async exportToCSV() {
+    if(this.results_mDex.length == 0) {
+      document.getElementById("info")!.innerHTML = `<span style="font-size: 2em; color: red;">There are no results!</span>`;
+    };
+
+    let csv_lines: string[] = [
+      'title,year,tags,link'
+    ];
+    for(const manga of this.results_mDex) {
       if(manga.attributes.availableTranslatedLanguages.includes("en")) continue;
 
       let title: string = manga.attributes.title.en;
@@ -168,7 +333,14 @@ let engine = new MangaDex();
 
 for (const el of document.getElementsByClassName("select-engine")) {
   el.addEventListener("change", (value) => {
-    console.log((value.target as HTMLInputElement).type)
+    console.log((value.target as HTMLInputElement).value)
+    if((value.target as HTMLInputElement).value == "mangaupdates") {
+      engine = new MangaUpdates()
+      engine.setTags();
+    } else {
+      engine = new MangaDex();
+      engine.setTags();
+    } 
   })
 }
 
@@ -185,10 +357,12 @@ document.getElementById("search-by-tags")?.addEventListener("click", async () =>
   let excluded: string[] = [];
 
   for(const opt of includedOptions) {
-    included.push(opt.value);
+    if(opt.value != "none")
+      included.push(opt.value);
   }
   for(const opt of excludedOptions) {
-    excluded.push(opt.value);
+    if(opt.value != "none")
+      excluded.push(opt.value);
   }
 
   await engine.searchByTags(
